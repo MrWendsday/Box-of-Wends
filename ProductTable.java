2fb23f8232b74947d961735317051c5bfeddd56a
ButMeChris <57100181+MyopicArgleBargle@users.noreply.github.com>
2020-02-13 14:11:14 -0500

    Delete ProductTable.java

--- ProductTable.java
+++
@@ -1,9 +0,0 @@
-public class ProductTable
-{
-	public static void main(String[] args)
-	{
-		for (int x = 1; x <= 10; x++)
-			for (int i = 1 ; i <= 10 ; i++)
-				System.out.println(x + " * " + i + " = " + x*i );
-	}
-}
